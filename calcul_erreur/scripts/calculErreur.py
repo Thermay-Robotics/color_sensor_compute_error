a2c58340093b47d9194d20c563f293afef9d7904
aymeP <58934999+aymeP@users.noreply.github.com>
2022-09-30 17:28:28 +0200

    Update comments

--- calcul_erreur/scripts/calculErreur.py
+++ calcul_erreur/scripts/calculErreur.py
@@ -4,7 +4,7 @@ from datetime import date
 import rospy
 from std_msgs.msg import Int16MultiArray,MultiArrayDimension,MultiArrayLayout,String,Float32
 
-
+#Global variables
 node = None
 nodePublisherColor = None
 nodePublisherError = None
@@ -30,13 +30,18 @@ error = 0
 
 threshold = 0
 
+
+#Callback function used when a message is received on the topic colorSensorValue 
 def callback(data):
     global threshold, orange_red, orange_green, orange_blue, white_avg, black_avg,cible,error, dark_blue_red, dark_blue_green, dark_blue_blue, light_blue_red, light_blue_green, light_blue_blue
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+
+    #Get the data
     data_red = data.data[0]
     data_green = data.data[1]
     data_blue = data.data[2]
 
+    #Check if the color is orange or blue, else compute the error
     if data_red > orange_red - threshold and data_red < orange_red + threshold and data_green > orange_green - threshold and data_green < orange_green + threshold and data_blue > orange_blue - threshold and data_blue < orange_blue + threshold:
         rospy.loginfo("[COLOR]orange")
         publishColor("orange")
@@ -52,7 +57,7 @@ def callback(data):
         publishError()
     
 
-
+#Function to get the parameters
 def calibrate():
     global orange_red, orange_green, orange_blue, white_avg, black_avg, threshold, cible, dark_blue_red, dark_blue_green, dark_blue_blue, light_blue_red, light_blue_green, light_blue_blue
     
@@ -72,6 +77,7 @@ def calibrate():
     cible = (white_avg + black_avg)/2
 
  
+#Function to initialize the node
 def init():
     global node, nodePublisherColor, nodePublisherError
     rospy.init_node('subscriber_node')
@@ -80,17 +86,21 @@ def init():
     nodePublisherError = rospy.Publisher("Error", Float32, queue_size=10)
     
 
+#Function to publish the error
 def publishError():
     global node,error
     data = error
     nodePublisherError.publish(data)
     rospy.loginfo(rospy.get_caller_id() + "I published %s", data)
 
+#Function to publish the color
 def publishColor(couleur):
     global node
     data = couleur
     nodePublisherColor.publish(couleur)
     rospy.loginfo(rospy.get_caller_id() + "I published %s", data)
+
+
 
 if __name__ == '__main__':
     init()
